e3f062b02c1c51cfd8f8aa4f0f178f90011948ab
Amshekh <hi.amitshekhar@gmail.com>
2023-07-09 19:00:17 +0530

    added jQuery programs

---
+++ 3_Using jQuery through external file/jq_functions.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {       // We are using Nesting Query method
+			$("button").click(function () {  // Syntax  $(documnt).ready(function () {.. jQuery methods.. }); 
+				$("p").hide();
+			});
+		});
